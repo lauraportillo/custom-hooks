c7b866e218d9ddf7e6c78df556407dc01f80d3c0
Laura Portillo <lauraportillo.ark@gmail.com>
2021-12-01 12:43:20 +0100

    create my custom hook useCounter

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,31 @@ import React, { useState } from 'react';
 // Styles
 import '../stylesheets/App.scss';
 
-const App = () => {
+// My Custom Hook
+const useCounter = () => {
   //state
   const [counter, setCounter] = useState(0);
+
+  const increase = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
+  return {
+    counter,
+    increase,
+    decrement,
+    reset,
+  };
+};
+
+const App = () => {
+  const { counter, increase, decrement, reset } = useCounter();
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <button onClick={() => setCounter(0)}>Reset</button>
+      <button onClick={increase}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
