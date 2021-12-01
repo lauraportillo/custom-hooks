e6bcd1e991fa024c6359c4ae3c4380ce880daa97
Laura Portillo <lauraportillo.ark@gmail.com>
2021-12-01 12:11:46 +0100

    add a working counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 // React
 import React, { useState } from 'react';
+// Styles
 import '../stylesheets/App.scss';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  //state
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
+    </div>
+  );
+};
 
 export default App;
