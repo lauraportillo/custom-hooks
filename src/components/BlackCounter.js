a90c76db54719a8b63f4fb72188ec5882749dc41
Laura Portillo <lauraportillo.ark@gmail.com>
2021-12-02 12:47:19 +0100

    create new compo

---
+++ src/components/BlackCounter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/BlackCounter.scss';
+
+const BlackCounter = (props) => {
+  return (
+    <>
+      <div>hola soy un contador negro</div>
+    </>
+  );
+};
+
+export default BlackCounter;
