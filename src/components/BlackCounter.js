b527622d562ab70bb3aaa798658716339cea1339
Laura Portillo <lauraportillo.ark@gmail.com>
2021-12-03 13:33:31 +0100

    finish the exercise

--- src/components/BlackCounter.js
+++ src/components/BlackCounter.js
@@ -7,15 +7,21 @@ import '../stylesheets/BlackCounter.scss';
 const BlackCounter = (props) => {
   const { counter, increase, decrement, reset } = useCounter(10);
   return (
-    <>
-      <h2>Black counter</h2>
-      <div>
-        <div>{counter}</div>
-        <button onClick={increase}>+</button>
-        <button onClick={decrement}>-</button>
-        <button onClick={reset}>Reset</button>
+    <section className="black">
+      <h2 className="black__title">Black counter</h2>
+      <div className="black__counter">{counter}</div>
+      <div className="black__btns">
+        <button className="black__btns--item" onClick={increase}>
+          +
+        </button>
+        <button className="black__btns--item" onClick={decrement}>
+          -
+        </button>
+        <button className="black__btns--item" onClick={reset}>
+          Reset
+        </button>
       </div>
-    </>
+    </section>
   );
 };
 
